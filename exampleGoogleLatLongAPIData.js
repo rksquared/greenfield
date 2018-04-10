2e5415893e1b3e35a7b2e1bdab1281456e05cffc
brianlee <blee@nyu.edu>
2018-04-09 20:08:14 -0400

    Added sample lat long Google API data

---
+++ exampleGoogleLatLongAPIData.js
@@ -0,0 +1,191 @@
+// example data using Hack Reactor's lat/lon, radius=50, type=bank, keyword=chase
+
+{
+   "html_attributions" : [],
+   "results" : [
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 40.7501328,
+               "lng" : -73.97649899999999
+            },
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 40.75156217989272,
+                  "lng" : -73.97534632010728
+               },
+               "southwest" : {
+                  "lat" : 40.74886252010727,
+                  "lng" : -73.97804597989273
+               }
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/bank_dollar-71.png",
+         "id" : "f61aa06fa3fa83e690d7fe9a45a35948be67d457",
+         "name" : "Chase Bank",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 500,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/100338243655446815049/photos\"\u003eChase Bank\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRaAAAAlOxgrj1ozpwfeE2-AWTp0Cu2MeFEv-c5mMyXGejvKVrdq55yxSyVWAfEcYbuDvrkIrPGLvFLTdyKOk5Jo1p4oGXkYmS7bXQ943le2TvMlTfH2WQN_oOeCq8ZnN7j7KWHEhBOj89y4NvBWSDEsAlk5nbyGhSCipbvI_5Xkl5CljoaymaiKAfIOQ",
+               "width" : 500
+            }
+         ],
+         "place_id" : "ChIJY9UK5gNZwokR60pPEpS1WKE",
+         "rating" : 3,
+         "reference" : "CmRbAAAA15izBEG5rgpL90SUrQ2WhvuEpsYu682wvispvTUnLKliX96WllLVMVvIthgRWQ0XXceLcVlt0YdpdplWoT_bnFcFsOOR7kBrOgRTqJ9NrC_jB9mRegzjyjatDZkIcdAdEhCun6fNnhnZYRZWgYAZ8kSLGhRqdZHehF_xsmXTWzLlXPtfm48caQ",
+         "scope" : "GOOGLE",
+         "types" : [ "bank", "atm", "finance", "point_of_interest", "establishment" ],
+         "vicinity" : "355 Lexington Ave, New York"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 40.749611,
+               "lng" : -73.974712
+            },
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 40.75114382989273,
+                  "lng" : -73.97352277010728
+               },
+               "southwest" : {
+                  "lat" : 40.74844417010728,
+                  "lng" : -73.97622242989273
+               }
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/bank_dollar-71.png",
+         "id" : "5914272df8bc523641c996317f8e977d4daec360",
+         "name" : "Chase Bank",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 3024,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/107222874427432551066/photos\"\u003eChristopher N Flores\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRaAAAAoTG4ef0FP9iItn8ft7mcJF8ye4NCoQJerTTqEFfNdvs-isJ6TUpSJJ4BvzDbToQbdHg5ZSsFtWdHa8-2OQaswsthO0eyVtWhlS2F1rQlSKF60QIPYatUClSYUKhz5bBNEhBUkOrlIaKJYjvHVesnup9qGhRdy2ZMNXwshsv9CUpg53DAkakRGg",
+               "width" : 4032
+            }
+         ],
+         "place_id" : "ChIJt5cK5gNZwokR4S9PcCtCTcQ",
+         "rating" : 4.5,
+         "reference" : "CmRbAAAAD7yYvbTiY7aUaGY8Nd6fe2EM9RV2dhVOcoGS8JX5uZOuNZhKuhipuwelupsJy2CZci6k_sQKZRVklsvS7cQslIZySgBSFhib_VTpYVyd01RBG4s_-oj4kiThdRpQcIvFEhBS1E1x4okhVnbuKIGE3G3rGhQr8JB_-etNUJvZTNN9D8vIW3EOxA",
+         "scope" : "GOOGLE",
+         "types" : [ "bank", "atm", "finance", "point_of_interest", "establishment" ],
+         "vicinity" : "633 3rd Ave, New York"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 40.7523476,
+               "lng" : -73.97863339999999
+            },
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 40.75376507989272,
+                  "lng" : -73.97721677010728
+               },
+               "southwest" : {
+                  "lat" : 40.75106542010727,
+                  "lng" : -73.97991642989271
+               }
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/bank_dollar-71.png",
+         "id" : "b7302ca4d6a3ee3261e1641cbac55cf03c186118",
+         "name" : "Chase Bank",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 1308,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/105170431124683218627/photos\"\u003eMarc Gonzalez\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRaAAAA3FfhycUmSseD7IvCvNVvgfJSlQAGKPcwLYULUi-rlrAKBKNUnS1mJX16B7YzzNAgQd-lnJZYe5uwnGo3RSpjS44iiIgmbLnzLPRQKc53jLPvowa75mvAONuGxtaVtJxIEhDIAAWjs_T4RBavPWshF7rSGhTWXsEzkuq5BX1QXjvfgnc6XMclXw",
+               "width" : 1926
+            }
+         ],
+         "place_id" : "ChIJt5cK5gNZwokRhY1eWF9Wwdg",
+         "rating" : 4.7,
+         "reference" : "CmRbAAAARe5gEjZmdsyrHukh1dMcRlbtAqKELfZ3jh1mHhB-3thfshdg8UHLrSFkgqdBJyu-FjGgrMkwt77IElazPVjpB3-OL7FILZc-kgABNZUqB5U7c3PJr7cvB3XZ-3jfKjBvEhB-LOtZMVP7WJaDIGJW-u3nGhTpAl9jOmd_AdmTtySEpYePBLDlgA",
+         "scope" : "GOOGLE",
+         "types" : [ "bank", "atm", "finance", "point_of_interest", "establishment" ],
+         "vicinity" : "60 E 42nd St, New York"
+      }
+   ],
+   "status" : "OK"
+}
+
+// Results for location is 146 Biscayne Road (in lat/lon), radius=500 (meters), type=supermarket, no keyword (i.e. no brand)
+
+{
+   "html_attributions" : [],
+   "results" : [
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 25.7720699,
+               "lng" : -80.1891564
+            },
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 25.7734226802915,
+                  "lng" : -80.18765641970849
+               },
+               "southwest" : {
+                  "lat" : 25.7707247197085,
+                  "lng" : -80.19035438029151
+               }
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png",
+         "id" : "ad9157900e974d16d700ac1fd5977239c8c6c9f1",
+         "name" : "Whole Foods Market",
+         "opening_hours" : {
+            "open_now" : true,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 3024,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/114915296802233085336/photos\"\u003eRodolfo Barron\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRaAAAArnyixRfjxz69-pIoQYLW9wemYFXY_8-714PqLX4MrcythM7RiVNimSIS3YSE3Saj1kR2N37ilnF_QFMAOItwiZqY3wxdovqpAQS9Js0pkYE7s1LuBVHjvZkjSmeWK3ukEhBHrmKk8dr69yzb6wRgiM_2GhQghHmY0JYmUR-wOvk5LNNLzTChew",
+               "width" : 4032
+            }
+         ],
+         "place_id" : "ChIJeWRcn5222YgRvTtLMdQCGy8",
+         "price_level" : 3,
+         "rating" : 4.6,
+         "reference" : "CmRRAAAAB5ZPKuzbFuYEmgklAvece9Zm2XvQtP7gkQ8BMsp-oD0McpuB6Ljka8YtrDRg9SNT049giIQ1Jr3uGyaaDu5lj-uZOmkNalqT1-sFi0Ltgte29kjGdE-IfwWGG2x1Q54iEhAOM1qqXnZQMO8rvGzvuuieGhRjDfynpcCRXtwkgwgYAiFRhIg_Mw",
+         "scope" : "GOOGLE",
+         "types" : [
+            "grocery_or_supermarket",
+            "supermarket",
+            "store",
+            "health",
+            "food",
+            "point_of_interest",
+            "establishment"
+         ],
+         "vicinity" : "299 Southeast 3rd Avenue, Miami"
+      }
+   ],
+   "status" : "OK"
+}
+
