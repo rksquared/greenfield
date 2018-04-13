9d12fcfabf412671aca8eb4009ed7bc493bbc52d
Ana Vasquez <anvasquez08@gmail.com>
2018-04-13 15:49:37 -0400

    Added dummy data for saved favorites.

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -14,7 +14,60 @@ class Search extends React.Component {
 		this.state = {
 			address: '',
 			results: [],
-			favorites: [],
+			favorites: [
+  [
+    { distance: '0.4 mi',
+      travel_time: '4 mins',
+      type: 'bank',
+      place_lat: 40.7501328,
+      place_long: -73.97649899999999,
+      category_icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/bank_dollar-71.png',
+      google_id: 'ChIJY9UK5gNZwokR60pPEpS1WKE',
+      place_name: 'Chase Bank',
+      rating: 3,
+      place_address: '355 Lexington Ave, New York',
+      thumbnail: '<a href="https://maps.google.com/maps/contrib/100338243655446815049/photos">Chase Bank</a>',
+      price_level: '' },
+    { distance: '0.5 mi',
+      travel_time: '5 mins',
+      type: 'bank',
+      place_lat: 40.749611,
+      place_long: -73.974712,
+      category_icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/bank_dollar-71.png',
+      google_id: 'ChIJt5cK5gNZwokR4S9PcCtCTcQ',
+      place_name: 'Chase Bank',
+      rating: 4.5,
+      place_address: '633 3rd Ave, New York',
+      thumbnail: '<a href="https://maps.google.com/maps/contrib/107222874427432551066/photos">Christopher N Flores</a>',
+      price_level: '' }
+  ],
+  [
+    { distance: '0.3 mi',
+      travel_time: '3 mins',
+      type: 'supermarket',
+      place_lat: 40.7522358,
+      place_long: -73.9756911,
+      category_icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png',
+      google_id: 'ChIJnaciOAJZwokR2eQCKWId9B8',
+      place_name: 'Eli Zabar\'s',
+      rating: 1,
+      place_address: '89 East 42nd Street, New York',
+      thumbnail: '<a href="https://maps.google.com/maps/contrib/101941738990436117323/photos">Gregory Weiss</a>',
+      price_level: '' },
+    { distance: '0.7 mi',
+      travel_time: '6 mins',
+      type: 'supermarket',
+      place_lat: 40.74663890000001,
+      place_long: -73.977746,
+      category_icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png',
+      google_id: 'ChIJw7VKFwRZwokRVJgwZnK-eEY',
+      place_name: 'D\'Agostino',
+      rating: 3.2,
+      place_address: '528 3rd Avenue, New York',
+      thumbnail: '<a href="https://maps.google.com/maps/contrib/114435253726526221846/photos">Dijon Davantes</a>',
+      price_level: '' }
+  ]
+],
 			swapFaves: false
 		}
 		//this.props.location.prefs is the prefs object
@@ -35,13 +88,9 @@ class Search extends React.Component {
 
 		axios.post('/places', {params: params})
 		.then(({data}) => {
-			console.log('places fetched')
-			console.log('whats the response', data)
-			// will need to update result state with data
 			this.setState({
 				results: [data]
 			});
-			
 		})
 		.catch((err) => console.log('error in fetching places'))
 	}
@@ -60,7 +109,16 @@ class Search extends React.Component {
 			create_time: new Date()
 		};
 		axios.post('/save', {params: params})
-			.then((resp) => console.log('resp after saving is', resp))
+			.then((resp) => {
+					
+				/*let fav = JSON.parse(resp.config.data)
+				let newfaveState = this.state.favorites.slice();
+				newfaveState.push(fav.params)
+
+				this.setState({
+					favorites: newfaveState
+				});*/
+		})
 			.catch((err) => console.log('err trying to save fave is', err))
 	}
 
